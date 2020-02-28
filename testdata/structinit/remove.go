3648976edf055cbe9ab9b34a773bdd9177174b35
Anton Stöckl <anton.stoeckl@maibornwolff.de>
2020-02-28 17:06:13 +0100

    Saving messed up whitespace somehow

--- testdata/structinit/remove.go
+++ testdata/structinit/remove.go
@@ -3,14 +3,14 @@
 package example
 
 type Some struct {
-	a string
-	B int
+	a	string
+	B	int
 }
 
 func removeFieldsFromStructInit() Some {
 	some := Some{
-		a: "a value",
-		B: 5,
+		a:	"a value",
+		B:	5,
 	}
 
 	return some
